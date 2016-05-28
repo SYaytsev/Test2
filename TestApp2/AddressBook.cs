617bf4b03cf0c1a2fc3ec419d9b13c85ea27c524
SYaytsev <yaytsevs@gmail.com>
2016-05-29 01:27:49 +0300

    User, Logger, AddressBokk, Program classes added

---
+++ TestApp2/AddressBook.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp2
+{
+    /// <summary>
+    /// AddressBook - класс, который хранит список юзеров (User).
+    /// Список пользователей не доступен вне класса.
+    /// AddressBook должен иметь методы AddUser, RemoveUser и события UserAdded, UserRemoved.
+    /// </summary>
+    class AddressBook
+    {
+        private List<User> users;
+
+        public AddressBook()
+        {
+            this.users = new List<User>();
+        }
+
+        public bool AddUser(User in_user)
+        {
+            try
+            {
+                users.Add(in_user);
+                UserAdded("User " + in_user.ToString() + " - was added to address book");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                UserNotAdded("User " + in_user.ToString() + " - was not added to address book " + ex.Message);
+                return false;
+            }
+        }
+        public bool RemoveUser(User in_user)
+        {
+            if (users.Remove(in_user))
+            {
+                UserRemoved("User " + in_user.ToString() + " - was removed from address book");
+                return true;
+            }
+            else
+            {
+                UserNotRemoved("User " + in_user.ToString() + " - was not removed from address book");
+                return false;
+            }
+
+        }
+        public delegate void EventHandler(string str);
+
+        public event EventHandler UserAdded;
+        public event EventHandler UserRemoved;
+        public event EventHandler UserNotAdded;
+        public event EventHandler UserNotRemoved;
+    }
+}
---
+++ TestApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp2
+{
+    /// <summary>
+    /// Program - класс для тестирования Logger AddressBook.
+    /// </summary>
+    class Program
+    {
+        private static Logger log;
+
+        static void Main(string[] args)
+        {
+            log = Logger.Instance(Strategy.RecordToFile);
+
+            AddressBook addressBook = new AddressBook();
+            log.Debug("Address Book was created");
+
+            addressBook.UserAdded += log.Info;
+            addressBook.UserRemoved += log.Info;
+
+            addressBook.UserNotAdded += log.Error;
+            addressBook.UserNotRemoved += log.Error;
+
+            User user1 = new User("Fedorov", "Ivan", new DateTime(1990, 7, 13), "Kiev");
+            User user2 = new User("Iovcheva", "Irina", new DateTime(1970, 7, 3), "Odessa");
+            log.Debug("2 users were initialized");
+
+            if (!addressBook.AddUser(user1)) { log.Warning("Program class was informed that user had not been added"); }
+            if (!addressBook.RemoveUser(user2)) { log.Warning("Program class was informed that user had not been removed"); }
+
+            if (!addressBook.AddUser(user2)) { log.Warning("Program class was informed that user had not been added"); }
+            if (!addressBook.RemoveUser(user2)) { log.Warning("Program class was informed that user had not been removed"); }
+
+            try
+            {
+                addressBook = null;
+                addressBook.ToString();
+            }
+            catch (Exception ex)
+            {
+                log.Error("You can not return a string of null reference: " + ex.Message);
+            }
+
+            log.Dispose();
+        }
+    }
+}
